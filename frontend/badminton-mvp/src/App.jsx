a9301ed5bf294a6723f58db77c50b479c3c53607
Ryan <168155235+ikar888@users.noreply.github.com>
2026-01-14 20:41:08 +0800

    Reapply "Merge branch 'feat/landing-page-navbar' into 'main'"
    This reverts commit 02cfe6c4efd5fef4e84e443eaecfa846376fe79a.

--- frontend/badminton-mvp/src/App.jsx
+++ frontend/badminton-mvp/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import LandingPage from "./pages/LandingPage";
+import Root from "./utils/Root";
+import HomePage from "./pages/HomePage";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Route>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoutes>
+              <HomePage />
+            </ProtectedRoutes>
+          }
+        ></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
