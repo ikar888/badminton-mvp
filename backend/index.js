d7da834cc0145b02647311ad25ef1048885020e6
Ryan <ikar_888@yahoo.com>
2026-01-14 10:53:18 +0000

    Revert "Merge branch 'feat/landing-page-navbar' into 'main'"
    This reverts merge request !10

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./database/connectDB.js";
-import cors from "cors";
 import paymentPlayerRoutes from "./routes/paymentPlayerRoutes.js"
 
 const app = express();
@@ -12,12 +11,6 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
