b0c12e3b64d783656dfb2dbe38b3966e4bbe335f
Mik30 <baomikaela@gmail.com>
2026-01-17 22:58:55 +0800

    feat: update files

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,19 @@ import connectDB from "./database/connectDB.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import paymentGMRoutes from "./routes/paymentGMRoutes.js";
-import paymentPlayerRoutes from "./routes/paymentPlayerRoutes.js"
+import paymentPlayerRoutes from "./routes/paymentPlayerRoutes.js";
 import webhookRoutes from "./routes/webhook.js";
+import analysisRoutes from "./routes/analysisRoutes.js";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 connectDB();
 
-app.use("/webhook", express.raw({ type: 'application/json' }), webhookRoutes); 
+app.use("/webhook", express.raw({ type: "application/json" }), webhookRoutes);
 app.use(express.json());
 app.use(
   cors({
@@ -32,6 +36,7 @@ app.use("/api/v1/matches", matchRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/payments/gm", paymentGMRoutes);
 app.use("/api/v1/payments/player", paymentPlayerRoutes);
+app.use("/api/v1/video", analysisRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Badminton-MVP");
